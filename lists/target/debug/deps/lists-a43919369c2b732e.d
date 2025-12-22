79f8e910c354ffc81390699482f9e1e5c2dec9ad
Sungsu Ahn <youkioh97@gmail.com>
2025-12-22 22:32:42 +0900

    first list

---
+++ lists/target/debug/deps/lists-a43919369c2b732e.d
@@ -0,0 +1,8 @@
+/home/sungsu/rust_study/lists/target/debug/deps/lists-a43919369c2b732e.d: src/lib.rs src/first.rs
+
+/home/sungsu/rust_study/lists/target/debug/deps/liblists-a43919369c2b732e.rlib: src/lib.rs src/first.rs
+
+/home/sungsu/rust_study/lists/target/debug/deps/liblists-a43919369c2b732e.rmeta: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
---
+++ lists/target/debug/liblists.d
@@ -0,0 +1,1 @@
+/home/sungsu/rust_study/lists/target/debug/liblists.rlib: /home/sungsu/rust_study/lists/src/first.rs /home/sungsu/rust_study/lists/src/lib.rs
