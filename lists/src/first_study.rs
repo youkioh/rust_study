4738a56b1f7f5434d6c33138b6203e123dbba424
Sungsu Ahn <youkioh97@gmail.com>
2025-12-23 17:48:38 +0900

    review by myself

---
+++ lists/src/first_study.rs
@@ -0,0 +1,69 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty}
+    }
+
+    pub fn push(&mut self, elem:i32) {
+        let new_node = Box::new(Node{
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut curr_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = curr_link {
+            println!("{:?}", boxed_node.elem);
+            curr_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        } 
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn basics() {
+        use super::List;
+
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+    }
+}
--- lists/src/lib.rs
+++ lists/src/lib.rs
@@ -1,1 +1,3 @@
 pub mod first;
+
+pub mod first_study;
