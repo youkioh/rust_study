449aa24ce4ee3e32c416b9a59cbc53b7840bbc99
Sungsu Ahn <youkioh97@gmail.com>
2025-12-23 15:48:13 +0900

    Update .gitignore and remove target/ from tracking

--- lists/target/debug/deps/lists-f3817fb32fbd1d88.d
+++
@@ -1,6 +0,0 @@
-/home/sungsu/rust_study/lists/target/debug/deps/lists-f3817fb32fbd1d88.d: src/lib.rs src/first.rs
-
-/home/sungsu/rust_study/lists/target/debug/deps/lists-f3817fb32fbd1d88: src/lib.rs src/first.rs
-
-src/lib.rs:
-src/first.rs:
--- lists/target/debug/liblists.d
+++
@@ -1,1 +0,0 @@
-/home/sungsu/rust_study/lists/target/debug/liblists.rlib: /home/sungsu/rust_study/lists/src/first.rs /home/sungsu/rust_study/lists/src/lib.rs
